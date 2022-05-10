1ea320ccc057cb91739f0389436617fb2786348c
Garret Poole <garretpoole@users.noreply.github.com>
2022-05-09 21:55:00 -0700

    User interaction disabled w allowsHitTesting(), and different based on contentShape()

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -5,62 +5,29 @@
 //  Created by Garret Poole on 5/9/22.
 //
 
-import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    @State private var engine: CHHapticEngine?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-            .onAppear(perform: prepareHaptics)
-            .onTapGesture(perform: complexSuccess)
-        
-    }
-    
-    func simpleSuccess() {
-        let gernerator = UINotificationFeedbackGenerator()
-        gernerator.notificationOccurred(.success)
-    }
-    
-    func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        
-        do {
-            engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("There was an error creating the enginer: \(error.localizedDescription)")
-        }
-    }
-    
-    func complexSuccess() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        
-        var events = [CHHapticEvent]()
-        
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
-            //transient is quick event
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
-            events.append(event)
-        }
-        
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1-i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1-i))
-            //transient is quick event
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1+i)
-            events.append(event)
-        }
-        
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play pattern \(error.localizedDescription)")
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 300, height: 300)
+                .onTapGesture {
+                    print("Rectangle Tapped")
+                }
+            //same frame but only 'taps' when touch circle itself
+            Circle()
+                .fill(.red)
+                .frame(width: 300, height: 300)
+                //makes entire frame count as tap area
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    print("Circle Tapped")
+                }
+                //tap on circle is ignored
+//                .allowsHitTesting(false)
         }
     }
 }
