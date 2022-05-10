61775bbfc44dfe08d5ddd06b7cc732f13544f2d4
Garret Poole <garretpoole@users.noreply.github.com>
2022-05-09 20:48:23 -0700

    Default gesture priority and simultaneous gestures

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,34 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var currentAmount = Angle.zero
-    @State private var finalAmount = Angle.zero
+    @State private var offset = CGSize.zero
+    @State private var isDragging = false
     
     var body: some View {
-        Text("Hello, world!")
-//            .onTapGesture(count: 2) {
-//                print("Double Tapped")
-//            }
-//            .onLongPressGesture(minimumDuration: 1) {
-//                print("Long Press")
-//            } onPressingChanged: { inProgress in
-//                print("In progress: \(inProgress)")
-//            }
         
-//            .scaleEffect(finalAmount + currentAmount)
-            .rotationEffect(finalAmount + currentAmount)
-            .gesture(
-//                MagnificationGesture()
-                RotationGesture()
-                    .onChanged { angle in
-                        currentAmount = angle
-                    }
-                    .onEnded { angle in
-                        finalAmount +=  currentAmount
-                        currentAmount = .zero
-                    }
-            )
         
+        VStack {
+            //child gesture is given priority by default
+            Text("Hello, world!")
+                .onTapGesture {
+                    print("Text Tapped")
+                }
+                
+        }
+//        .onTapGesture {
+//            print("VStack Tapped")
+        //overrides default child priority
+//        .highPriorityGesture(
+        //makes gestures work together (no override)
+        .simultaneousGesture(
+            TapGesture()
+                .onEnded {
+                    print("VStack Tapped")
+                }
+        )
         
     }
 }
