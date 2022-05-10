e73cc04a06f1de376cca3bd947c6a2eae8ceb471
Garret Poole <garretpoole@users.noreply.github.com>
2022-05-09 20:40:13 -0700

    Different gestures (magnify, rotate, long press)

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentAmount = Angle.zero
+    @State private var finalAmount = Angle.zero
+    
     var body: some View {
         Text("Hello, world!")
-            .padding()
+//            .onTapGesture(count: 2) {
+//                print("Double Tapped")
+//            }
+//            .onLongPressGesture(minimumDuration: 1) {
+//                print("Long Press")
+//            } onPressingChanged: { inProgress in
+//                print("In progress: \(inProgress)")
+//            }
+        
+//            .scaleEffect(finalAmount + currentAmount)
+            .rotationEffect(finalAmount + currentAmount)
+            .gesture(
+//                MagnificationGesture()
+                RotationGesture()
+                    .onChanged { angle in
+                        currentAmount = angle
+                    }
+                    .onEnded { angle in
+                        finalAmount +=  currentAmount
+                        currentAmount = .zero
+                    }
+            )
+        
+        
     }
 }
 
