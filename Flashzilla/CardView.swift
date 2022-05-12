4eaf72e6d6e29cec6f03c11c83ef043b4a8507f9
Garret Poole <garretpoole@users.noreply.github.com>
2022-05-12 13:58:58 -0700

    Moving view with DragGesture() and offset(); stack of cards now 'rises' on card removal

--- Flashzilla/CardView.swift
+++ Flashzilla/CardView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct CardView: View {
     let card: Card
+    var removal: (() -> Void)? = nil
     
     @State private var showingAnswer = false
+    @State private var offset = CGSize.zero
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
@@ -33,6 +36,24 @@ struct CardView: View {
         }
         //means fully visible no matter the iphone
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(Double(offset.width*0.2)))
+        .offset(x: offset.width * 5, y: 0)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    offset = gesture.translation
+                }
+                .onEnded { _ in
+                    if abs(offset.width) > 100 {
+                        //remove card
+                        removal?()
+                        
+                    } else {
+                        offset = .zero
+                    }
+                }
+        )
         .onTapGesture {
             showingAnswer.toggle()
         }
