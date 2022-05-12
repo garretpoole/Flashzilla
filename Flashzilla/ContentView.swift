abfc47d2a6cc9dbaf1caf1d6ab871d5ce71f2266
Garret Poole <garretpoole@users.noreply.github.com>
2022-05-12 13:28:25 -0700

    Start of project

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -6,24 +6,12 @@
 //
 
 import SwiftUI
-//allows us to not repeat animation code in body
-func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-    if UIAccessibility.isReduceMotionEnabled {
-        return try body()
-    } else {
-        return try withAnimation(animation, body)
-    }
-}
 
 struct ContentView: View {
-    @Environment(\._accessibilityReduceTransparency) var reduceTransparency
 
     var body: some View {
         Text("Hello World")
             .padding()
-            .background(reduceTransparency ? .black : .black.opacity(0.5))
-            .foregroundColor(.white)
-            .clipShape(Capsule())
     }
 }
 
